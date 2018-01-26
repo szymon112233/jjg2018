c8834c498631cdae13e45c69826bc3cc49583189
nanoblit <nanoblit@users.noreply.github.com>
2018-01-26 22:51:32 +0100

    basic Game, money manager, Game Box tag

--- GJEGJEJOT2018/Assets/Scripts/Game.cs
+++ GJEGJEJOT2018/Assets/Scripts/Game.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
+    public string Name = "Generic Game";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float ProgrammingValue = 0f;
+    public float MusicValue = 0f;
+    public float ArtValue = 0f;
+    public float TestValue = 0f;
 }
---
+++ GJEGJEJOT2018/Assets/Scripts/GameReceiver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameReceiver : MonoBehaviour
+{
+    public string BoxTag;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag(BoxTag))
+        {
+            Game game = GetComponent<Game>();
+        }
+    }
+
+}
