fada25281e753ee6acb31591612f8a60e0284424
Piotr Żebrowski <piotrek@agairad.net>
2018-01-26 23:31:48 +0100

    + [Player] Added player that walks and throws game boxes around * [GameBox] Added rigidbody to gamebox

---
+++ GJEGJEJOT2018/Assets/Scripts/Controller.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	public float MaxSpeed = 0.2f;
+	public float ThrowSpeed = 2.0f;
+	private CharacterController controller;
+	private GameObject gameSocket;
+	private GameObject dropSocket;
+	// Use this for initialization
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		foreach (Transform child in transform)
+		{
+			if (child.gameObject.tag == "GameSocket")
+			{
+				gameSocket = child.gameObject;
+			}
+			if (child.gameObject.tag == "DropSocket")
+			{
+				dropSocket = child.gameObject;
+			}
+		}
+	}
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		var body = hit.gameObject;
+		if (body.tag != "Game Box")
+			return;
+		GameObject child = DetachGame();
+		if (child != null)
+		{
+			child.transform.position = dropSocket.transform.position;
+		}
+		Rigidbody rigid = body.GetComponent<Rigidbody>();
+		rigid.useGravity = false;
+		rigid.detectCollisions = false;
+		body.transform.SetParent(gameSocket.transform);
+		body.transform.rotation = Quaternion.identity;
+		rigid.velocity = rigid.angularVelocity = body.transform.localPosition = Vector3.zero;
+	}
+
+	private GameObject DetachGame()
+	{
+		if (gameSocket.transform.childCount == 0)
+			return null;
+		var child = gameSocket.transform.GetChild(0).gameObject;
+		var rigid = child.GetComponent<Rigidbody>();
+		rigid.detectCollisions = true;
+		rigid.useGravity = true;
+		gameSocket.transform.DetachChildren();
+		return child;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		Vector3 moveVector = new Vector3(x, 0, y);
+		controller.Move(moveVector * MaxSpeed * Time.deltaTime);
+		if (moveVector.magnitude > 0)
+		{
+			transform.LookAt(transform.position + moveVector);
+		}
+		if (Input.GetAxis("Fire1") > 0)
+		{
+			var game = DetachGame();
+			if(game != null)
+			{
+				var rigid = game.GetComponent<Rigidbody>();
+				if(rigid != null)
+				{
+					rigid.velocity = ThrowSpeed * transform.localToWorldMatrix.MultiplyVector(Vector3.forward);
+				}
+			}
+		}
+	}
+}
