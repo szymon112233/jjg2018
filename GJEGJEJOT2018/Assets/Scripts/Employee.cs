b77433f7357070e391f821afd42e705c07ec27b9
Speego <zbigniew.m.andrzejewski@gmail.com>
2018-01-26 21:48:41 +0100

    employees draft

---
+++ GJEGJEJOT2018/Assets/Scripts/Employee.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Employee : MonoBehaviour {
+
+    protected Ability programing;
+    protected Ability musicSpeed;
+    protected Ability artSpeed;
+    protected Ability testSpeed;
+
+    private Game _game;
+
+    protected virtual void Update()
+    {
+        if (_game != null)
+        {
+            WorkOnGame();
+        }
+            
+    }
+
+    protected virtual void WorkOnGame()
+    {
+
+    }
+
+    public void SetGame(Game game, )
+}
