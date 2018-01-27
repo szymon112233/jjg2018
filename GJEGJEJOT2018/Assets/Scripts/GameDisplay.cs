6e9dd6e6a9c5b2d7c237ff1674469c36e62fcaf4
nanoblit <nanoblit@users.noreply.github.com>
2018-01-27 12:46:27 +0100

    GameDisplay, MoneyDisplay

---
+++ GJEGJEJOT2018/Assets/Scripts/GameDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDisplay : MonoBehaviour
+{
+    public Text GameTitle;
+    public Slider ProgrammingBar;
+    public Slider ArtBar;
+    public Slider MusicBar;
+    public Slider TestingBar;
+    public Image TaskImage;
+
+    public void SetGameDisplay(string name, Color color, float programming, float music, float art, float testing)
+    {
+        GameTitle.text = name;
+        GameTitle.color = color;
+        ProgrammingBar.value = programming;
+        ArtBar.value = art;
+        MusicBar.value = music;
+        TestingBar.value = testing;
+    }
+
+    public void SetTask(TaskEnum currentTask)
+    {
+        // set task image
+    }
+}
