6678aa6b465659759b08de8255e16eab70fb0932
Piotr Żebrowski <piotrek@agairad.net>
2018-01-28 13:42:46 +0100

    + Credits camera

--- GJEGJEJOT2018/Assets/Scripts/CameraInterpolater.cs
+++ GJEGJEJOT2018/Assets/Scripts/CameraInterpolater.cs
@@ -7,25 +7,40 @@ public class CameraInterpolater : MonoBehaviour
 {
 	Transform startPos;
 	Transform endPos;
+	private Transform creditPos;
 	new Camera camera;
 	private MoneyManager money;
 	private Controller player;
 	private GameSpawner spawner;
 	private Canvas canvas;
+	private MenuCanvas menu;
 	public float InterpTime = 5f;
 	private float elaspedTime = 0;
 	bool play = false;
+	bool credits = false;
+	bool backCredits = false;
+
+
 	// Use this for initialization
 	void Start()
 	{
 		startPos = GameObject.FindGameObjectWithTag("CameraStartPos").transform;
 		endPos = GameObject.FindGameObjectWithTag("CameraEndPos").transform;
+		creditPos = GameObject.FindGameObjectWithTag("CameraCreditsPos").transform;
 		camera = GetComponent<Camera>();
 
 		money = GameObject.FindObjectOfType<MoneyManager>();
 		player = GameObject.FindObjectOfType<Controller>();
 		spawner = GameObject.FindObjectOfType<GameSpawner>();
 		canvas = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<Canvas>();
+		menu = GameObject.FindObjectOfType<MenuCanvas>();
+	}
+
+	public void ShowCredits()
+	{
+		elaspedTime = 0;
+		credits = true;
+		menu.Hide();
 	}
 
 	public void BeginPlay() //#UE4
@@ -45,19 +60,64 @@ public class CameraInterpolater : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if (backCredits)
+		{
+			elaspedTime += Time.deltaTime / InterpTime;
+
+			float effectiveTime = elaspedTime - 0.1f;
+			camera.transform.position = Vector3.Lerp(creditPos.position, startPos.position, effectiveTime);
+			camera.transform.rotation = Quaternion.Slerp(creditPos.rotation, startPos.rotation, effectiveTime);
+
+			if (effectiveTime > 1.10f)
+			{
+				backCredits = false;
+				elaspedTime = 0;
+				menu.Show();
+			}
+			return;
+		}
+
+		if (credits)
+		{
+			if (elaspedTime - 0.1f > 1.10f && Input.GetButtonDown("Cancel"))
+			{
+				ShowReturnButton();
+				return;
+			}
+			elaspedTime += Time.deltaTime / InterpTime;
+
+			float effectiveTime = elaspedTime - 0.1f;
+			camera.transform.position = Vector3.Lerp(startPos.position, creditPos.position, effectiveTime);
+			camera.transform.rotation = Quaternion.Slerp(startPos.rotation, creditPos.rotation, effectiveTime);
+
+			return;
+		}
 		if (!play)
 			return;
 
-
-		elaspedTime += Time.deltaTime / InterpTime;
-
-		float effectiveTime = elaspedTime - 0.1f;
-		camera.transform.position = Vector3.Lerp(startPos.position, endPos.position, effectiveTime);
-		camera.transform.rotation = Quaternion.Slerp(startPos.rotation, endPos.rotation, effectiveTime * 3f);
-
-		if (effectiveTime > 1.2f)
 		{
-			StartGsme();
+			elaspedTime += Time.deltaTime / InterpTime;
+
+			float effectiveTime = elaspedTime - 0.1f;
+			camera.transform.position = Vector3.Lerp(startPos.position, endPos.position, effectiveTime);
+			camera.transform.rotation = Quaternion.Slerp(startPos.rotation, endPos.rotation, effectiveTime * 3f);
+
+			if (effectiveTime > 1.10f)
+			{
+				StartGsme();
+			}
 		}
+	}
+
+	private void ShowReturnButton()
+	{
+		credits = false;
+		elaspedTime = 0;
+		ReturnToMainMenuFromCredits();
+	}
+
+	private void ReturnToMainMenuFromCredits()
+	{
+		backCredits = true;
 	}
 }
